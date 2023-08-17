cdfa3f29790f7bfe0281213826268c410810733d
sushmitha <sushmitha.connect@gmail.com>
2023-08-17 14:19:50 +0530

    Program to add two numbers

---
+++ add_two_numbers.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter num1 value : ")) #request input from user for operand1
+num2 = int(input("Enter num2 value : ")) #request input from user for operand2
+output = num1 + num2 #add operand1 and operand2 and store it in variable output
+print(output) #print the value stored in variable output
